b451244160cd2f4befd8ccf0c8e8e02c49229ed1
seburoh <seburoh@outlook.com>
2023-07-28 20:01:01 -0700

    data collection added

--- README.md
+++ README.md
@@ -3,6 +3,37 @@ Wolf Simulation for University of Washington Tacoma TCSS435: Artificial Intellig
 
 The simulation has been updated to include multiple sliders for adjusting behaviors of predators and prey, as well as updating the included code to the modern js object notation.
 
+# Data Collection Mode
+It is possible to upload data to the database by following these instructions:
+* Enter values:
+    * Run Count: How many runs to do with the same settings.
+    * Target Tick: What tick to end the simulation at, inclusive.
+* Set slider values to desired positions.
+* Check the box for Freeze Visual if desired for compute power.
+* Click "DataSim".
+
+The simulation will then run for the desired runs, to the ticks. Position data for each entity in the simulation will be recorded every 100 ticks, to be uploaded at the end. Some basic information is given in the console while this is running, to note what run number is currently in progress.
+
+There is a line at the end of *main.simUpload()* which is commented out, if uncommented the data from each run will also be displayed in the console.
+
+When the runs are complete, the simulation will revert to normal and perform a normal simulation without data collection enabled.
+
+The shape of data recorded and uploaded is as follows:
+```js
+    var data = {
+        db: "tcss435",
+        collection: "red3",
+        data: {
+            runType: 'test',
+            runNumber: params.runCount, //integer of which run is being uploaded
+            sliders: params.slider, //values of the alignment, cohesion, and separation sliders
+            preyList: params.isPrey, //a list of which entities are prey (true) or predator (false)
+            positions: params.posData //2d list containing JSON marking x and y positions of each entity
+        }
+    };
+```
+Basic interaction with the data can be seen in the debug print functions at the end of *main*.
+
 # Resources
 * <a href="https://github.com/algorithm0r/WolfPack">WolfPack</a>: Base simulation this project forks from.
 
--- main.js
+++ main.js
@@ -23,10 +23,18 @@ ASSET_MANAGER.downloadAll(() => {
     document.getElementById("reset").addEventListener("click", () => {
         reset();
     });
+
+    document.getElementById("start_sim").addEventListener("click", () => {
+        startSim();
+    });
+
     gameEngine.init(ctx);
     gameEngine.start();
 });
 
+/**
+ * Start simulation without hooks for data collection.
+ */
 function reset() {
     gameEngine.entities = [];
 
@@ -45,4 +53,146 @@ function reset() {
     gameEngine.weakened.debug = true;
     gameEngine.weakened.MaxSpeed *= 0.70;
     gameEngine.weakened.color = "teal";
+}
+
+/**
+ * Connect to remote database.
+ */
+function databaseConnectSetup() {
+    socket = io.connect('http://73.225.31.4:8888'); //connect out
+    
+    // socket.on("find", function (array) {
+    //     if (array.length > 0) parseData(array);
+    //     else console.log("Empty data.");
+    // });
+
+    params.dbConnectSuccess = true; //log that we've successfully connected
+}
+
+/**
+ * Start simulation with data hooks for data collection.
+ */
+function startSim() {
+    //first run setup
+    if (!params.simMode) {
+
+        //verify user isn't a meanie
+        var runCountInput = document.getElementById("numberInput").value;
+        if (runCountInput == '' || isNaN(runCountInput) || runCountInput < 1) {
+            console.log('Invalid run count input');
+            return;
+        }
+
+        var targetTickInput = document.getElementById("tickTarget").value;
+        if (targetTickInput == '' || isNaN(targetTickInput) || targetTickInput < 100) {
+            console.log('Invalid tick count input');
+            return;
+        }
+
+        //turn on data collection mode
+        params.simMode = true; //ensure sim mode is on
+        params.runTarget = runCountInput;
+        params.tickTarget = targetTickInput;
+        params.runCount = 0;
+
+        //setup socket stuff if not previously performed
+        if (!params.dbConnectSuccess) {
+            databaseConnectSetup();
+        }
+
+        //save the slider values this set of runs is for
+        params.slider = { //grab this run's slider values
+            cohesion: parseFloat(document.getElementById("coh_weight").value),
+            alignment: parseFloat(document.getElementById("ali_weight").value),
+            separation: parseFloat(document.getElementById("sep_weight").value)
+        };
+
+    } else { //mode already engaged, begin next run
+        params.runCount++;
+    }
+
+    console.log('Starting run #' + params.runCount);
+
+    //we do this both when a new setup happens, and when a new run happens
+    params.posData = []; //build/empty list for positional data lists
+    params.isPrey = [];
+    params.insertCol = 0; //where in the lists we insert data
+    gameEngine.entities = [];
+    gameEngine.ticks = 0;
+
+    for (var i = 0; i < 50; i++) {
+        circle = new Circle(gameEngine, true);
+        gameEngine.addEntity(circle);
+        params.posData[i] = []; //build list for positional data
+        params.isPrey[i] = true;
+    }
+    for (var i = 0; i < 5; i++) {
+        circle = new Circle(gameEngine, false);
+        gameEngine.addEntity(circle);
+        params.posData[i + 50] = []; //build list for positional data
+        params.isPrey[i + 50] = false;
+    }
+
+    gameEngine.weakened = gameEngine.entities[0];
+    gameEngine.weakened.debug = true;
+    gameEngine.weakened.MaxSpeed *= 0.70;
+    gameEngine.weakened.color = "teal";
+}
+
+/**
+ * Upload results to DB.
+ */
+function simUpload() {
+    console.log('Uploading run #' + params.runCount);
+    //collect data in JSON to yeet out
+    var data = {
+        db: "tcss435",
+        collection: "red3",
+        data: {
+            runType: 'test',
+            runNumber: params.runCount,
+            sliders: params.slider,
+            preyList: params.isPrey,
+            positions: params.posData
+        }
+    };
+
+    //yeet
+    // socket.emit("insert", data);
+    printDataDebug(data);
+}
+
+/**
+ * Below are print functions for data to ensure shape is correct.
+ * @param {params.posData} positions 
+ */
+function printDataDebug(data) {
+    // console.log('data: ' + data.data.positions);
+    console.log('d: ' + data.db);
+    console.log('c: ' + data.collection);
+    console.log('da: ' + data.data);
+    console.log('rt: ' + data.data.runType);
+    console.log('rn: ' + data.data.runNumber);
+    console.log('sl: ' + data.data.sliders);
+    console.log('sl: ' + data.data.preyList);
+    testData(data.data.positions);
+}
+
+function testData(positions) {
+    for (let i = 0; i < positions.length; i++) {
+        var thisLine = 'i:'+i+': ';
+        for (let j = 0; j < positions[i].length; j++) {
+            thisLine += '{' + debugGet(positions[i][j]) + '}, ';
+        }
+        console.log(thisLine);
+    }
+}
+
+function debugGet(obj) {
+    return ('x: ' + obj.x + ', y: ' + obj.y);
+}
+
+function debugDisp(obj) {
+    var j = params.insertCol;
+    console.log('x: ' + obj[j].x + ', y: ' + obj[j].y);
 }
