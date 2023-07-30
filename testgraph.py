18c1b44cb8d489d9126ccc14d9fb6dbb3d67d165
Jeep Naarkom <maimalai@gmail.com>
2023-07-29 19:34:30 -0700

    update lowest5, avedistance calculation

--- testgraph.py
+++ testgraph.py
@@ -14,8 +14,8 @@ print("starting")
 with open ("data3.json", "r") as json_file:
     data = json.load(json_file)
 closestPrey =[]
-isPrey = data[2].get("preyList")
-allPosition = data[2].get("positions")
+isPrey = data[0].get("preyList")
+allPosition = data[0].get("positions")
 # split wolvese and sheep array based on the true/false
 preyArr = []  #50
 wolfArr = []  #5
@@ -33,44 +33,51 @@ for i in range(len(isPrey)):
 # print(len(allPosition[0]))
 #----end-------------------
 
-
-# loop through prey list if true, find position of the prey
+# loop through all ticks
+# loop through prey list to find distance of all sheep
 # loop through predicator (prey list if false) find positon of all preditors
 # for each preditor, find the position of the prey nearest
 catchDistance = []
 index = []
-var1 = 0
-var2 = 0 
-var3 = 0
-averageDistance = 0
+
+#calulate for every tick
 for i in range(len(preyArr[2])):
     
-    for j in range(len(preyArr[2])):    
+    alldistance = []  
+    lowest5 = []
+    averageDistance = 0.0
+    
+    for j in range(len(preyArr[2])):  
+        dist = 0        
         dist = distance(preyArr[2][i].get("x"), preyArr[2][i].get("y"), 
                     wolfArr[2][j].get("x"), wolfArr[2][j].get("y"))
-        # print(dist)
-        if dist <= var1:
-            var1 = dist
-        elif dist > var1 or dist <= var2:
-            var2 = dist
-        elif dist > var2 or dist <= var3:
-            var3 = dist
-            
-        averageDistance = (var1 + var2 + var3)/3
-        # print(averageDistance)
+        
+        alldistance.append(dist)
+    # print("alldistance: " + str(len(alldistance)))
+    
+    lowest5 = sorted(alldistance)[:5]
+    print(lowest5)
+    averageDistance = sum(lowest5)
+    averageDistance = averageDistance/len(lowest5)
+    averageDistance = round(averageDistance, 2)
+    # print("avedistance: " + str(averageDistance))
+    
     catchDistance.append(averageDistance)
 
     # print(catchDistance)
     index.append(i)
     # print(len(catchDistance))
-print(catchDistance)
+# print(catchDistance)
 
 # test = prey_position 
 # print(test)
 
-plt.plot(index, catchDistance)
-plt.xlabel ("ticks")
-plt.ylabel ("average distance of closest sheep")
-plt.title("Catch Distance")
-plt.show()
+def plotgraph():
+    plt.plot(index, catchDistance)
+    plt.xlabel ("per 100 ticks")
+    plt.ylabel ("average distance of closest sheep")
+    plt.title("Catch Distance")
+    plt.show()
+
+plotgraph()
 
