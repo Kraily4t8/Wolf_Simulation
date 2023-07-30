0c9c73f72886cc9cfee98c2c36c2cd98ab51e68f
Kraily4t8 <tomtien2002@outlook.com>
2023-07-29 21:51:45 -0700

    finally understanding the code I think
    The measured ticks are found in position[i]

--- graphdata.js
+++ graphdata.js
@@ -18,19 +18,29 @@ function calculateDistance(point1, point2) {
 }
 
 // INCORRECT: Function for finding average minimum distances.
-//*check distance between wolf and closest sheep
+// The average (the average distance between each wolf and the closest sheep IN ONE TICK) across all runs
 function findAverageMinimumDistances(data) {
-    const distances = [];
+    const avgDistancesAcrossAllRuns = [];
+    const avgDistancesForEachRun = [];
 
+    //forEach of the 3 runs
     data.forEach(run => {
+        //console.log("Data length " + data.length); //data length 3
         const {preyList, positions} = run;
-        const preyCount = preyList.filter(Boolean).length;
-        const predatorCount = preyList.length - preyCount;
+
+        const preyCount = preyList.filter(Boolean).length; //50
+        const predatorCount = preyList.length - preyCount; //5
 
         if (preyCount === 0 || predatorCount === 0) return;
 
         const minDistances = [];
+        console.log("positions0 length " + positions[0].length); //251 measured ticks
+        //console.log("positions0 " + positions[0]); // the xy tuples of positions
+        //positions = each entity info, l = 55, 
+        //positions[0] = all positions recorded, l = 251
         
+        //measuredTick tracking
+
         for(let i = 0; i < 50; i++){
             //for each prey
             positions[i].forEach(prey => {
@@ -39,30 +49,49 @@ function findAverageMinimumDistances(data) {
                 //index 50 to 54 are predators
                 //for each of the 5 predators
                 positions.slice(50).forEach(predators => {
-                    //confusing
+                    //for each predator, determine how far is the closest prey
                     predators.forEach(predator => {
                         const distance = calculateDistance(prey, predator);
                         minDistance = Math.min(minDistance, distance);
                     });
                 });
-
+                
                 minDistances.push(minDistance);
             });
         }
 
+        for(let i = 0; i < 50; i++){
+            //for each prey
+            for(let measuredTick = 0; measuredTick < positions[i].length; measuredTick++) {
+                let minDistance = Number.MAX_VALUE;
+
+                //index 50 to 54 are predators
+                //for each of the 5 predators
+                positions.slice(50).forEach(predators => {
+                    //for each predator, determine how far is the closest prey
+                    predators.forEach(predator => {
+                        const distance = calculateDistance(prey, predator);
+                        minDistance = Math.min(minDistance, distance);
+                    });
+                });
+                
+                minDistances.push(minDistance);
+            }
+        }
+
         //reduce uses a callback funciton, need further assistance to understand
         const averageMinDistance = minDistances.reduce((acc, val) => acc + val, 0) / minDistances.length;
-        distances.push(averageMinDistance);
+        avgDistancesForEachRun.push(averageMinDistance);
     });
 
-    return distances;
+    return avgDistancesForEachRun;
 }
 
 // Function to plot the data, probably correct.
 function plotData(data) {
-    const timeSteps = data[0].positions[0].length;
+    const timeSteps = data[0].positions[0].length; //251
     const averageDistances = findAverageMinimumDistances(data);
-
+    console.log("avgDistancesForEachRun " + averageDistances); //3
     const trace = {
         x: timeSteps,
         y: averageDistances,
