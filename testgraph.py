a6ffcfcb77d8d21dff733a7e68f4bb978441ec9a
Jeep Naarkom <maimalai@gmail.com>
2023-07-29 19:49:15 -0700

    swap to runNumber = 0, of the goodTest

--- testgraph.py
+++ testgraph.py
@@ -34,24 +34,19 @@ for i in range(len(isPrey)):
 #----end-------------------
 
 # loop through all ticks
-# loop through prey list to find distance of all sheep
-# loop through predicator (prey list if false) find positon of all preditors
-# for each preditor, find the position of the prey nearest
+# loop through prey list to find 5 closest sheep to wolf then take average distance
+
 catchDistance = []
 index = []
-
 #calulate for every tick
 for i in range(len(preyArr[2])):
-    
     alldistance = []  
     lowest5 = []
     averageDistance = 0.0
-    
     for j in range(len(preyArr[2])):  
         dist = 0        
-        dist = distance(preyArr[2][i].get("x"), preyArr[2][i].get("y"), 
-                    wolfArr[2][j].get("x"), wolfArr[2][j].get("y"))
-        
+        dist = distance(preyArr[0][i].get("x"), preyArr[0][i].get("y"), 
+                    wolfArr[0][j].get("x"), wolfArr[0][j].get("y"))
         alldistance.append(dist)
     # print("alldistance: " + str(len(alldistance)))
     
